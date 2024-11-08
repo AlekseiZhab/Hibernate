d201c721dde363d627870fe8559a4acad226d22f
Aleksei <aleksei.zh@mail.ru>
2024-11-08 23:23:18 +0300

    method-security

---
+++ src/main/java/ru/netology/hibernate/controller/MethodSecurityController.java
@@ -0,0 +1,32 @@
+package ru.netology.hibernate.controller;
+
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MethodSecurityController {
+    @Secured({"ROLE_READ"})
+    @GetMapping("/read")
+    public String getRead() {
+        return "Read";
+    }
+    @RolesAllowed({"ROLE_WRITE"})
+    @GetMapping("/write")
+    public String getWrite() {
+        return "Write";
+    }
+    @PreAuthorize("hasAnyRole('WRITE', 'DELETE')")
+    @GetMapping("/wd")
+    public String getWriteOrDelete() {
+        return "Write or Delete";
+    }
+    @GetMapping("/")
+    @PostAuthorize("#username == authentication.principal.username")
+    public String greetingUser(String username) {
+        return "Hi, " + username;
+    }
+}
--- src/main/java/ru/netology/hibernate/security/SecurityConfig.java
+++ src/main/java/ru/netology/hibernate/security/SecurityConfig.java
@@ -3,11 +3,14 @@ package ru.netology.hibernate.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder encoder() {
@@ -18,11 +21,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("Aleksei")
                 .password(encoder().encode("1234"))
-                .authorities("read", "write")
+                .roles("READ", "WRITE", "DELETE")
                 .and()
                 .withUser("Koly")
                 .password("{noop}5678")
-                .authorities("read");
+                .roles("READ")
+                .and()
+                .withUser("Ivan").password(encoder().encode("8765"))
+                .roles("WRITE");
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
